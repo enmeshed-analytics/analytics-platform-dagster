357c2b1a9f60beacb0becfdbea0c9a6e0c6656ed
christophercarlon <cmcarleone@gmail.com>
2024-06-21 17:57:01 +0100

    21.06.2024: defined asset, job, and definition. EEnsured they all all run one after the other and write to same duckdb

--- analytics_platform_dagster/__init__.py
+++ analytics_platform_dagster/__init__.py
@@ -1,9 +1,9 @@
-from dagster import Definitions, load_assets_from_modules
+from dagster import Definitions
 
-from . import assets
-
-all_assets = load_assets_from_modules([assets])
+from .jobs.analytics_platfom_job_1 import assets_job_1
+from .assets.analytics_platform_assets_1 import dbt_trade_barriers, ea_flood_areas, ea_floods, os_open_usrns
 
 defs = Definitions(
-    assets=all_assets,
+    assets=[dbt_trade_barriers, ea_flood_areas, ea_floods, os_open_usrns],
+    jobs=[assets_job_1]
 )
--- analytics_platform_dagster/assets/analytics_platform_assets_1.py
+++ analytics_platform_dagster/assets/analytics_platform_assets_1.py
@@ -6,14 +6,18 @@ import geopandas as gpd
 import zipfile
 import tempfile
 import os
+import time
 
 from shapely.ops import transform
 from loguru import logger
-from dagster import asset
+from dagster import asset, AssetIn, OpExecutionContext
 
+def wait_10_seconds(context: OpExecutionContext):
+    time.sleep(10)
+    context.log.info("Waited for 10 seconds")
 
 @asset
-def dbt_trade_barriers():
+def dbt_trade_barriers(context: OpExecutionContext):
     # Get data from api and create dataframe
     url = 'https://data.api.trade.gov.uk/v1/datasets/market-barriers/versions/latest/data?format=json'
     response = requests.get(url)
@@ -27,46 +31,47 @@ def dbt_trade_barriers():
     # Create a table and insert the data
     con.execute('CREATE TABLE IF NOT EXISTS trade_barriers AS SELECT * FROM df')
     print("Data stored in the 'trade_barriers' table.")
+    wait_10_seconds(context)
 
-
-@asset()
-def ea_flood_areas():
+@asset(deps=[dbt_trade_barriers])
+def ea_flood_areas(context: OpExecutionContext):
     # Get data from api and create dataframe
     url = 'https://environment.data.gov.uk/flood-monitoring/id/floodAreas?_limit=9999'
     response = requests.get(url)
     response.raise_for_status()
     data = response.json()
     df = pd.DataFrame(data['items'])
+    print(df.head())
 
     # Create a connection to a persistent DuckDB database file named "data"
     con = duckdb.connect('data.duckdb')
 
     # Create a table and insert the data
     con.execute('CREATE TABLE IF NOT EXISTS ea_flood_areas AS SELECT * FROM df')
-    print("Data stored in the 'trade_barriers' table.")
-
+    print("Data stored in the 'ea_flood_areas' table.")
+    wait_10_seconds(context)
 
 @asset(deps=[ea_flood_areas])
-def ea_floods():
+def ea_floods(context: OpExecutionContext):
     # Get data from api and create dataframe
     url = 'https://environment.data.gov.uk/flood-monitoring/id/floods'
     response = requests.get(url)
     response.raise_for_status()
     data = response.json()
     df = pd.DataFrame(data['items'])
+    print(df.head())
 
     # Create a connection to a persistent DuckDB database file named "data"
     con = duckdb.connect('data.duckdb')
 
     # Create a table and insert the data
     con.execute('CREATE TABLE IF NOT EXISTS ea_floods AS SELECT * FROM df')
-    print("Data stored in the 'trade_barriers' table.")
-    
+    print("Data stored in the 'ea_floods' table.")
+    wait_10_seconds(context)
 
-@asset()
-def os_open_usrns():
-    
-    db_file='openusrn.duckdb'
+@asset(deps=[ea_floods])
+def os_open_usrns(context: OpExecutionContext):
+    db_file='data.duckdb'
     num_rows=20
     url = "https://api.os.uk/downloads/v1/products/OpenUSRN/downloads?area=GB&format=GeoPackage&redirect"
     
@@ -106,12 +111,9 @@ def os_open_usrns():
                     break
 
             # Step 7: Load the specified number of rows from the GeoPackage into a GeoDataFrame
-            # Change this in the future so it reads in a batch and keeps reading after to process the full gpkg file
             if gpkg_file:
                 try:
                     gdf = gpd.read_file(gpkg_file, rows=num_rows)
-                    # This works but returns a warning: 'Geometry column does not contain geometry' - think this is just a bug and there's a couple of github issues on it
-                    # the to_wkt() successfully makes the geometry col in well known text...
                     gdf['geometry'] = gdf['geometry'].apply(lambda geom: transform(lambda x, y, z: (x, y), geom))
                     gdf['geometry'] = gdf['geometry'].to_wkt()
                     gdf.info()
@@ -126,7 +128,7 @@ def os_open_usrns():
                         con.execute('INSTALL spatial; LOAD spatial;')
                         # Create a table from the GeoPackage file
                         table_name = 'open_usrns_table'
-                        con.execute(f"CREATE TABLE {table_name} AS SELECT * FROM gdf;")
+                        con.execute(f"CREATE TABLE IF NOT EXISTS {table_name} AS SELECT * FROM gdf;")
                         # Query the table in the DuckDB database
                         query = f"""
                         SELECT usrn, geometry
@@ -139,12 +141,10 @@ def os_open_usrns():
                     logger.error(f"Error executing DuckDB operations: {e}")
                     raise
 
+                wait_10_seconds(context)
                 return None
             else:
                 raise FileNotFoundError("No GeoPackage file found in the zip archive")
     except Exception as e:
         logger.error(f"Error processing the GeoPackage: {e}")
         raise
-
-# Example usage
-# os_open_usrns()
