da85dbde0206c5604eb5a1d71ee24c926e4bcda0
christophercarlon <cmcarleone@gmail.com>
2024-07-27 15:05:45 +0100

    Updates 2024-07-27 - Updates

--- analytics_platform_dagster/assets/infrastructure_data_assets/analytics_platform_national_charge_points_assets.py
+++ analytics_platform_dagster/assets/infrastructure_data_assets/analytics_platform_national_charge_points_assets.py
@@ -1,23 +1,23 @@
-from ...utils.requests_helper import return_api_data_json
-from ...utils.io_manager import S3JSONManager
-from ...models.infrastructure_data_models.national_charge_point_model import ChargeDeviceResponse
-from datetime import datetime
-from dagster import asset, AssetExecutionContext, AssetIn
+# from ...utils.requests_helper import return_api_data_json
+# from ...utils.io_manager import S3JSONManager
+# from ...models.infrastructure_data_models.national_charge_point_model import ChargeDeviceResponse
+# from datetime import datetime
+# from dagster import asset, AssetExecutionContext, AssetIn
 
-API_ENDPOINT = "https://ukpowernetworks.opendatasoft.com/api/explore/v2.1/catalog/datasets/ozev-ukpn-national-chargepoint-register/records?limit=50"
+# API_ENDPOINT = "https://ukpowernetworks.opendatasoft.com/api/explore/v2.1/catalog/datasets/ozev-ukpn-national-chargepoint-register/records?limit=50"
 
-def fetch_national_charge_point_data() -> ChargeDeviceResponse:
-    url = API_ENDPOINT
-    data = return_api_data_json(url)
-    return ChargeDeviceResponse.model_validate(data, strict=True)
+# def fetch_national_charge_point_data() -> ChargeDeviceResponse:
+#     url = API_ENDPOINT
+#     data = return_api_data_json(url)
+#     return ChargeDeviceResponse.model_validate(data, strict=True)
 
-@asset(
-    group_name="energy_assets", 
-    io_manager_key="S3Parquet"
-    )
-def national_charge_point_data_bronze():
-    response = fetch_national_charge_point_data()
-    return response
+# @asset(
+#     group_name="energy_assets", 
+#     io_manager_key="S3Parquet"
+#     )
+# def national_charge_point_data_bronze():
+#     response = fetch_national_charge_point_data()
+#     return response
 
 
 # def national_charge_point_data_silver():
