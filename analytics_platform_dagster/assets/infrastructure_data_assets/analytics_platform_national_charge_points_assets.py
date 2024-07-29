bbea1d67eb76805769c99c191a6fb58ebd72b2b7
christophercarlon <cmcarleone@gmail.com>
2024-07-29 17:51:47 +0100

    Added record count check

--- analytics_platform_dagster/assets/infrastructure_data_assets/analytics_platform_national_charge_points_assets.py
+++ analytics_platform_dagster/assets/infrastructure_data_assets/analytics_platform_national_charge_points_assets.py
@@ -1,15 +1,35 @@
+import json
 
-from typing import Any
-from ...utils.requests_helper.requests_helper import stream_json
+from typing import List, Dict
+from ...utils.requests_helper.requests_helper import stream_json, return_json
 from ...utils.variables_helper.url_links import asset_urls
 # from ...models.infrastructure_data_models.national_charge_point_model import ChargeDeviceResponse
 from dagster import AssetExecutionContext, asset
 
-DONWLOADLINK = asset_urls.get("national_charge_points")
+DONWLOAD_LINK = asset_urls.get("national_charge_points")
+API_ENDPOINT = asset_urls.get("national_charge_points_api")
 
-def fetch_national_charge_point_data() -> Any:
+def fetch_record_count() -> json:
+    """Returns sample of charge point data to verify total count
+    
+    Returns: 
+        Json object
+    """
     try:
-        url = DONWLOADLINK
+        api_endpoint = API_ENDPOINT 
+        json_data = return_json(api_endpoint)
+        return json_data.get("total_count")
+    except Exception as error:
+        raise error
+
+def fetch_national_charge_point_data() -> List[Dict]:
+    """Streams charge point data into memory
+    
+    Returns: 
+        A list of dictionaries
+    """
+    try:
+        url = DONWLOAD_LINK
         return stream_json(url, set_chunk=5000)
     except Exception as error:
         raise error
@@ -18,8 +38,24 @@ def fetch_national_charge_point_data() -> Any:
     group_name="infrastructure_assets",
     io_manager_key="S3Json" 
     )
-def national_charge_point_data_bronze(context: AssetExecutionContext) -> Any:
-    context.log.info("Started Processing Data")
+def national_charge_point_data_bronze(context: AssetExecutionContext) -> List[Dict]:
+    """ Uploads charge point data to S3 using IO Manager. 
+    
+    Fetches json data and compares with total record count to ensure that the correct amount of records was fetched. 
+    
+    Returns:
+        A list of dictionaries
+    """
+
     response = fetch_national_charge_point_data()
-    context.log.info("Finished Processing Data")
+    total_record_count = fetch_record_count()
+    response_count = len(response)
+    
+        # Compare counts using sets
+    if set([total_record_count]) == set([response_count]):
+        context.log.info(f"Record counts match: {total_record_count}")
+    else:
+        context.log.warning(f"Record count mismatch: Expected {total_record_count}, got {response_count}")
+
     return response
+
--- analytics_platform_dagster/utils/variables_helper/url_links.py
+++ analytics_platform_dagster/utils/variables_helper/url_links.py
@@ -3,5 +3,6 @@ asset_urls = {
     "ea_flood_areas":"https://environment.data.gov.uk/flood-monitoring/id/floodAreas?_limit=9999", 
     "ea_floods":"https://environment.data.gov.uk/flood-monitoring/id/floodAreas?_limit=9999",
     "london_data_store":"https://data.london.gov.uk/api/datasets/export.json", 
-    "national_charge_points": "https://ukpowernetworks.opendatasoft.com/api/explore/v2.1/catalog/datasets/ozev-ukpn-national-chargepoint-register/exports/json?lang=en&timezone=Europe%2FLondon"
+    "national_charge_points": "https://ukpowernetworks.opendatasoft.com/api/explore/v2.1/catalog/datasets/ozev-ukpn-national-chargepoint-register/exports/json?lang=en&timezone=Europe%2FLondon", 
+    "national_charge_points_api": "https://ukpowernetworks.opendatasoft.com/api/explore/v2.1/catalog/datasets/ozev-ukpn-national-chargepoint-register/records?limit=1"
 }
